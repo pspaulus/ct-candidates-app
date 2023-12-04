2eb698c6628d4ff0b08fcbc1ca116339dacb6c59
Oscar Villagran <oscar.villagran@hotmail.com>
2023-12-04 14:25:47 -0500

    Set up Routes

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    logger()->info('wtf');
-    
-    return view('welcome');
-});
+Route::get('/', [TasksController::class, 'index']);
+Route::get('/tasks', [TasksController::class, 'index']);
+Route::get('/tasks/create', [TasksController::class, 'create']);
+Route::post('/tasks', [TasksController::class, 'store']);
+Route::patch('/tasks/{id}', [TasksController::class, 'update']);
+Route::delete('/tasks/{id}', [TasksController::class, 'delete']);
