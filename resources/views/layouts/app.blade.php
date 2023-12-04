9ecec67fd45ee89e69028cd9bab30549076f3ddf
Oscar Villagran <oscar.villagran@hotmail.com>
2023-12-04 15:28:17 -0500

    Set styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Tareas</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <!-- Option 1: Include in HTML -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
   </head>
   <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,13 +3,35 @@
 @section('content')
   <h1>Lista de Tareas</h1>
 
+  <form method="POST" action="/tasks" class="d-flex justify-content-between row g-3 align-items-end">
+    <div class="form-group col">
+      @csrf
+      <label for="description">Nueva Tarea</label>
+      <input type="text" class="form-control" name="description" style="margin-bottom: 20px;" />
+    </div>
+
+    <div class="form-group col-auto">
+      <button type="submit" class="btn btn-primary" style="margin-bottom: 20px;">
+        Crear Tarea
+      </button>
+    </div>
+
+    @if($errors->any())
+      <div class="alert alert-danger" role="alert" style="margin-top: 20px;">
+        @foreach($errors->all() as $error)
+          {{ $error }}
+        @endforeach
+      </div>
+    @endif
+  </form>
+
   @foreach($tasks as $task)
     <div class="card @if($task->isCompleted()) border-success @endif" style="margin-bottom: 20px;">
       <div class="card-body d-flex justify-content-between">
         <p class="@if($task->isCompleted()) text-decoration-line-through @endif">
           {{ $task->description }}
           @if($task->isCompleted())
-            <span class="badge bg-success">Completo</span>
+            <span class="badge bg-success" style="margin-left: 10px;">Completo</span>
           @endif
         </p>
 
@@ -18,14 +40,14 @@
             @method('PATCH')
             @csrf
 
-            <button class="btn btn-secondary" type="submit">Completo</button>
+            <button class="btn btn-secondary" type="submit"><i class="bi bi-check"></i></button>
           </form>
         @else
           <form action="/tasks/{{ $task->id }}" method="POST">
             @method('DELETE')
             @csrf
 
-            <button class="btn btn-danger" type="submit">Eliminar</button>
+            <button class="btn btn-danger" type="submit"><i class="bi bi-x"></i></button>
           </form>
         @endif
       </div>
