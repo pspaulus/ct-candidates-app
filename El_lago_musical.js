c2df98b4be294b55b17edb58d077f9708abeb333
Humberto Guiracocha <humbrt75@outlook.es>
2024-06-13 19:01:40 -0500

    some texts are changed

--- El_lago_musical.js
+++ El_lago_musical.js
@@ -1,4 +1,4 @@
-const lista = [
+const songs = [
     ["brr", "fiu", "cric-cric", "brrah"],
     ["pep", "birip", "trri-trri", "croac"],
     ["bri-bri", "plop", "cric-cric", "brrah"],
@@ -8,18 +8,18 @@ function returnSound(sound) {
     let newList = [];
     let index = 0;
 
-    for (let i = 0; i < lista.length; i++) {
-        index = lista[i].indexOf(sound);
+    for (let i = 0; i < songs.length; i++) {
+        index = songs[i].indexOf(sound);
         if (index !== -1) {
-            newList = lista[i];
+            newList = songs[i];
             break;
         }
     }
     return newList.slice(index + 1);
 }
 
-const reproducir = ["brr", "birip", "plop", "croac", "brrah"];
+const tests = ["brr", "birip", "plop", "croac", "brrah"];
 
-reproducir.forEach((item) => {
-    console.log({ ingreso: item, reproduciendo: returnSound(item) });
+tests.forEach((item) => {
+    console.log({ test: item, song: returnSound(item) });
 });
